b46c39e4e1febe9b15e57cc6e2e40106aebfa825
JensLam12 <jesterlynn1986@gmail.com>
2024-01-26 16:47:06 -0800

    Added ap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    SharedModule,
+    GifsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
